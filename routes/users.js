1fa52f4ac0c048e68330642855b075a83f88d244
Thiago Brezinski <thiagobrez@gmail.com>
2019-07-22 15:03:10 -0300

    test(user): add integration tests for user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send([
+    {
+      id: '1',
+      name: 'Thiago'
+    },
+    {
+      id: '2',
+      name: 'Teste'
+    }
+  ]);
 });
 
 module.exports = router;
